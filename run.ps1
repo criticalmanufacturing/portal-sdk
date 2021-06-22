68b4f8853bc8d2a6116be07f2da0aefca2fcc5eb
Nuno Almeida <61638845+almeidev@users.noreply.github.com>
2021-06-22 14:52:52 +0100

    Added Version file (#6)
    * Added Version file
    
    * Minor improvement to run script
    
    Co-authored-by: Nuno Almeida <NunoAlmeida@criticalmanufacturing.com>

--- run.ps1
+++ run.ps1
@@ -1,4 +1,4 @@
 param (
 	$Configuration = 'Release'
 )
-Start-Process pwsh '-c', "cd $PSScriptRoot; .\import.ps1 -BuildAndPublish -Configuration $Configuration; sleep 100"
+Start-Process pwsh '-NoExit', '-c', "cd $PSScriptRoot; .\import.ps1 -BuildAndPublish -Configuration $Configuration; sleep -m 100"

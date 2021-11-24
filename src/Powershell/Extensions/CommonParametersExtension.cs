fc2f6c728eaa65f24afde02c429622df491514ea
Ana Rita Norinho <ritanorinho98@hotmail.com>
2021-11-24 15:07:41 +0000

    Set some parameters as mandatory

--- src/Powershell/Extensions/CommonParametersExtension.cs
+++ src/Powershell/Extensions/CommonParametersExtension.cs
@@ -16,12 +16,12 @@ namespace Cmf.CustomerPortal.Sdk.Powershell.Extensions
         {
             parameters = new List<RuntimeDefinedParameter>();
             parametersValue = new Dictionary<string, object>();
-            CreateRuntimeParameter("CustomerInfrastructureAgent", Resources.INFRASTRUCTURE_EXISTING_NAME_HELP, typeof(string));
+            CreateRuntimeParameter("CustomerInfrastructureName", Resources.INFRASTRUCTURE_EXISTING_NAME_HELP, typeof(string), true);
             CreateRuntimeParameter("Name", Resources.DEPLOYMENT_NAME_HELP, typeof(string));
             CreateRuntimeParameter("Description", Resources.DEPLOYMENT_DESCRIPTION_HELP, typeof(string));
             CreateRuntimeParameter("ParametersPath", Resources.DEPLOYMENT_PARAMETERSPATH_HELP, typeof(FileInfo));
-            CreateRuntimeParameter("EnvironmentType", Resources.DEPLOYMENT_ENVIRONMENTTYPE_HELP, typeof(EnvironmentType));
-            CreateRuntimeParameter("LicenseName", Resources.DEPLOYMENT_LICENSE_HELP, typeof(string));
+            CreateRuntimeParameter("EnvironmentType", Resources.DEPLOYMENT_ENVIRONMENTTYPE_HELP, typeof(EnvironmentType),defaultValue: EnvironmentType.Development);
+            CreateRuntimeParameter("LicenseName", Resources.DEPLOYMENT_LICENSE_HELP, typeof(string), true);
             CreateRuntimeParameter("DeploymentTargetName", Resources.DEPLOYMENT_PACKAGE_HELP, typeof(string), true);
             CreateRuntimeParameter("TemplateName", Resources.INFRASTRUCTURE_EXISTING_ENVIRONMENT_TEMPLATE_NAME_HELP, typeof(string));
             CreateRuntimeParameter("OutputDir", Resources.DEPLOYMENT_OUTPUTDIR_HELP, typeof(DirectoryInfo));
@@ -37,7 +37,7 @@ namespace Cmf.CustomerPortal.Sdk.Powershell.Extensions
             parametersValue.Add(parameter.Name, parameter.Value);
         }
 
-        public void CreateRuntimeParameter(string name, string helpMessage, Type type, bool mandatory = false)
+        public void CreateRuntimeParameter(string name, string helpMessage, Type type, bool mandatory = false, object defaultValue = null)
         {
             ParameterAttribute parameterAttribute = new ParameterAttribute
             {
@@ -48,7 +48,8 @@ namespace Cmf.CustomerPortal.Sdk.Powershell.Extensions
             {
                 IsSet = false,
                 Name = name,
-                ParameterType = type
+                ParameterType = type, 
+                Value = defaultValue
             };
             runtimeParameter.Attributes.Add(parameterAttribute);
             parameters.Add(runtimeParameter);

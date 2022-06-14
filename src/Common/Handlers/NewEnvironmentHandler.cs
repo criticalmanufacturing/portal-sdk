070a9483abcdd7774e527cf8ac6a03eacba82196
Francisco Maciel <FranciscoMaciel@criticalmanufacturing.com>
2022-06-14 11:17:53 +0100

    Fix build

--- src/Common/Handlers/NewEnvironmentHandler.cs
+++ src/Common/Handlers/NewEnvironmentHandler.cs
@@ -114,7 +114,7 @@ namespace Cmf.CustomerPortal.Sdk.Common.Handlers
                     // If the user defined a site, load it, otherwise the template site will be used
                     if (!string.IsNullOrEmpty(siteName))
                     {
-                        environmentSite = await _customerPortalClient.GetObjectByName<ProductSite>(siteName)
+                        environmentSite = await _customerPortalClient.GetObjectByName<ProductSite>(siteName);
                     }
                 }
 
